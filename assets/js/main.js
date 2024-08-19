7ce70ba7832cab2d75648175946a6e62df1a3137
Cesar Cornejo <cesar@MacBook-Pro-de-Cesar.local>
2024-08-19 14:13:21 -0300

    initial configuration

---
+++ assets/js/main.js
@@ -0,0 +1,308 @@
+"use strict";
+var arrLang = {
+	'English': {
+		'course1': 'Course 1 (Spanish)',
+		'course2': 'Course 2 (English)',
+		'course3': 'Course 3 (English)',
+		'course': 'Courses',
+		'lecturer1': 'Lecturers: Raúl Fervari and Carlos Areces',
+		'lecturer2': 'Lecturer: Joost-Pieter Katoen',
+		'lecturer3': 'Lecturer: Benjamin Kaminski',
+		'about': 'About RIO',
+		'aboutDesc': 'The Rio Summer School of Informatics is  organized yearly by the Computer Science Department of the National University of Rio Cuarto. Traditionally, the school is a week-long event taking place during the Argentinean summer, in February. The original goal of the school was to provide the students of the computer science careers of the National University of Rio Cuarto with an ample and up-to-date  view of the discipline, as well as to promote  the contact and interaction between students, professors and researchers of computer science. To that end, the school was designed to provide short and intensive courses taught by prominent researchers and professors in computer science; the social events during the school provide an excellent opportunity to connect students and professors from different institutions. Since the first edition of the school in 1994, it has become a traditional event for the computer science community in Argentina, and the region. More than 3000 students have assisted the school, many of them are now distinguished researchers, professors and professionals of informatics. The success of previous editions of the school made it possible to establish relationships between local research groups and international researchers; thus, improving the collaboration between Argentinean and international computer science  institutions.' +
+			'        The summer school has had as professors some of the most distinguished researchers in different areas of computer science.',
+		'organizer': 'Organizers',
+		'university': 'University of Rio Cuarto \n February 13, 2023',
+		'date': "February 13, 2023",
+		'home': 'Inicio',
+		'registration': 'Registration',
+		'program': 'Program',
+		'sponsors': 'Sponsors',
+		'lastEdition': 'Previous Rio',
+		'schedule': 'Schedule' ,
+		'venue': 'Venue' ,
+		'school': 'Summer School of Informatic Sciences',
+		'eventStart': 'School start in:',
+		'get': 'How to get?',
+		'venueDesc': 'The school is held on the campus of the National University of Rio Cuarto. It is located 7km away from the city center.\n' +
+			'To get there, there are taxi and remises companies that can carry out this section (we do not know the prices).\n' +
+			'In addition, there is a local bus service which covers this route. Local transport (SATCRC) has its own card system (SATBUS). You can get more information on the company page:',
+		'registration1': 'The fee that you must pay depends on your category and the number of courses to be taken.',
+		'registration2':'The categories considered, and the corresponding fee, are the following:',
+		'registration3':'The courses have no fee for students belonging to the U.N.R.C.',
+		'registration4': 'On the other hand, the graduates of the U.N.R.C. must pay the fee according to the Category "Students of Public Universities"',
+		'registration5': 'Registration will be available soon',
+		'registrationForm': 'To register you must complete the following',
+		'cat': 'Category',
+		'cat1': 'Students of Public Universities (Argentina)',
+		'cat2': 'Teachers and Researchers of National and Provincial Institutions',
+		'cat3': 'Professionals and others',
+		'onecourse': 'One course',
+		'twocourse': 'Two course',
+		'threecourse': 'Three course',
+
+		'monday': 'Monday',
+		'tuesday': 'Tuesday',
+		'wednesday': 'Wednesday',
+		'thursday': 'Thursday',
+		'friday': 'Friday',
+
+		'sponsorsDesc': 'The Rio Cuarto Summer School is supported by the European Union’s Horizon 2020 research and innovation programme under the Marie Skłodowska-Curie grant agreement No. 101008233 (MISSION)',
+		'scholarships1': 'The Rio Summer School of Informatics offers students registration scholarships for different courses.',
+		'scholarships2': 'The beneficiaries of these may attend the courses without any charge.',
+		'scholarships3': 'The requirements to apply for the scholarship are the following:',
+		'scholarships4': 'Be enrolled in a degree course at an Argentine University',
+		'scholarships5': '',
+		'scholarships6': 'To make your scholarship request, you must fill the following',
+		'scholarships7': 'In addition to completing the form, they must send a certificate of academic performance to',
+		'scholarships': 'Enrollment Scholarships',
+		'scholarshipsAlojamiento' : 'Accommodation scholarships',
+		'scholarships8': 'In addition, the Computer Science Summer School offers accommodation scholarships. To apply for a scholarship, you must complete the following',
+		'talks': 'Invited Speakers',
+		'talk1': 'Talk 1',
+		'talk2': 'Talk 2',
+		'talk3': 'Talk 3'
+	},
+	'Español': {
+		'course1': 'Curso 1 (Español)',
+		'course2': 'Curso 2 (Inglés)',
+		'course3': 'Curso 3 (Inglés)',
+		'course': 'Cursos',
+		'lecturer1': 'A cargo de Raúl Fervari y Carlos Areces',
+		'lecturer2': 'A cargo de Joost-Pieter Katoen',
+		'lecturer3': 'A cargo de Benjamin Kaminski',
+		'about': 'Sobre la RIO',
+		'aboutDesc': 'La Escuela de Informática de Verano de Río es organizada anualmente por el Departamento de Ciencias de la Computación de la Universidad Nacional de Río Cuarto. Tradicionalmente, la escuela es un evento de una semana que se lleva a cabo durante el verano argentino, en febrero. El objetivo original de la escuela fue brindar a los estudiantes de las carreras de informática de la Universidad Nacional de Río Cuarto una visión amplia y actualizada de la disciplina, así como promover el contacto y la interacción entre estudiantes, profesores e investigadores de informática. Con ese fin, la escuela fue diseñada para brindar cursos cortos e intensivos impartidos por destacados investigadores y profesores en ciencias de la computación; los eventos sociales durante la escuela brindan una excelente oportunidad para conectar a estudiantes y profesores de diferentes instituciones. Desde la primera edición de la escuela en 1994, se ha convertido en un evento tradicional para la comunidad informática de Argentina y la región. Más de 3000 estudiantes han asistido a la escuela, muchos de ellos ahora son destacados investigadores, profesores y profesionales de la informática. El éxito de ediciones anteriores de la escuela permitió establecer relaciones entre grupos de investigación locales e investigadores internacionales; mejorando así la colaboración entre instituciones informáticas argentinas e internacionales.' +
+			' La escuela de verano ha tenido como profesores a algunos de los más destacados investigadores en diferentes áreas de la informática.',
+		'organizer': 'Organizadores' ,
+		'university': 'Universidad de Rio Cuarto \n 13 de Febrero, 2023',
+		'date': "13 de Febrero, 2023",
+		'home': 'Home',
+		'registration': 'Registración',
+		'program': 'Programa',
+		'shcedule': 'Calendario' ,
+		'lastEdition': 'Rio 2020',
+
+		'sponsors': 'Sponsors',
+		'venue': 'Lugar' ,
+		'school': '28 Escuela de Verano de Ciencias Informáticas',
+
+		'index_registration':  'Para registrarse, complete el siguiente formulario',
+		'eventStart': 'La escuela comienza en:',
+		'get':'¿Cómo llegar?',
+		'venueDesc': 'La escuela se realiza en el campus de la Universidad Nacional de Rio Cuarto. Se encuentra a 7km de distancia del centro de la Ciudad.\n' +
+			'Para llegar existen empresas de taxis y remises que pueden realizar este tramo (desconocemos los precios).\n' +
+			'Además, se cuenta con un servicio de colectivos locales los cuales cubren este trayecto. El transporte local (SATCRC) tiene un sistema propio de tarjeta (SATBUS). Puede obtener más información en la página de la empresa:',
+
+		'registration1': 'El arancel que deberá abonar depende de su categoría y la cantidad de cursos a realizar.\n',
+		'registration2':'Las categorías consideradas, y el arancel correspondiente, son las siguientes:',
+		'registration3':'Los cursos no tienen arancel para los alumnos pertenecientes a la U.N.R.C.',
+		'registration4': 'Por otro lado, los egresados de la U.N.R.C. deberán abonar el arancel acorde la Categoría "Estudiantes de Universidades Públicas"',
+		'registration5': 'El formulario de inscripción estará disponible pronto.',
+		'registrationForm': 'Para realizar su inscripción debe completar el siguiente',
+		'cat': 'Categoría',
+		'cat1': 'Estudiantes de Universidades Publicas',
+		'cat2': 'Docentes e Investigadores de instituciones provinciales y nacionales',
+		'cat3': 'Profesionales y otros',
+		'onecourse': 'Un curso',
+		'twocourse': 'Dos curso',
+		'threecourse': 'Tres curso',
+
+		'monday': 'Lunes',
+		'tuesday': 'Martes',
+		'wednesday': 'Miercoles',
+		'thursday': 'Jueves',
+		'friday': 'Viernes',
+		'sponsorsDesc': 'La Escuela de Verano de Río Cuarto cuenta con el apoyo del programa de investigación e innovación Horizonte 2020 de la Unión Europea en el marco del acuerdo de subvención Marie Skłodowska-Curie No. 101008233 (MISSION)',
+		'scholarships1': 'La Escuela de Verano de Ciencias Informáticas ofrece a los estudiantes becas de inscripción a los diferentes cursos.',
+		'scholarships2': 'Los beneficiarios de estas podrán asistir a los cursos sin ningún tipo de cargo.',
+		'scholarships3': 'EL requisito para postularse a la beca es el siguiente:',
+		'scholarships4': 'Estar Inscripto en una carrera de grado o de posgrado en una Universidades públicas Argentina. En caso de estar inscripto en una carrera de grado, haber rendido al menos una materia.',
+		'scholarships6': 'Para realizar su pedido de beca, debe completar el siguiente',
+		'scholarships7': 'Además de completar el formulario deberán enviar un certificado de rendimiento académico a riobecas@dc.exa.unrc.edu.ar',
+		'scholarships': 'Becas de Inscripción',
+		'scholarshipsAlojamiento' : 'Becas de Alojamiento',
+		'scholarships8': 'Además la Escuela de Verano de Ciencias Informáticas ofrece becas de alojamiento. Para realizar el pedido de beca, debe completar el siguiente',
+		'talks': 'Charlas Invitadas',
+		'talk1': 'Charla 1',
+		'talk2': 'Charla 2',
+		'talk3': 'Charla 3'
+	}
+};
+// Process translation
+// $(function() {
+// 	$('.translate').click(function() {
+// 		var lang = $(this).attr('id');
+//
+// 		$('.lang').each(function(index, item) {
+// 			$(this).text(arrLang[lang][$(this).attr('key')]);
+// 		});
+// 	});
+// });
+
+
+
+$('.selectpicker').change(function () {
+
+	var lang = $('.selectpicker').val();
+	$('.lang').each(function(index, item) {
+		$(this).text(arrLang[lang][$(this).attr('key')]);
+	});
+});
+
+
+// function toggleCourse(id) {
+// 	$(`.school ul.courses li div#${id}`).toggle();
+// }
+
+	function toggleCourse(id) {
+	$(`.school ul.courses li div#${id}`).toggle();
+}
+
+	function toggleSpeakers(id) {
+	$(`.school ul.courses li div div#${id}`).toggle();
+}
+	function toggleSpeakers(id) {
+	$(`.school ul.courses li div#${id}`).toggle();
+}
+//
+//
+// $(function(){
+// 	$('.selectpicker').selectpicker(function() {
+// 		var lang = $(('.selectpicker').find('[value=English]')).attr('id');
+//
+// 		$('.lang').each(function(index, item) {
+// 			$(this).text(arrLang[lang][$(this).attr('key')]);
+// 		});
+// 	});
+// });
+
+
+/* ======= Header animation ======= */
+const header = document.getElementById('header');
+
+window.onload=function()
+{
+    headerAnimation();
+
+};
+
+window.onresize=function()
+{
+    headerAnimation();
+
+};
+
+window.onscroll=function()
+{
+    headerAnimation();
+
+};
+
+
+function headerAnimation () {
+
+    var scrollTop = window.scrollY;
+
+	if ( scrollTop > 100 ) {
+	    header.classList.add('header-shrink');
+
+	} else {
+	    header.classList.remove('header-shrink');
+	}
+
+};
+
+/* ===== Smooth scrolling ====== */
+/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
+/* Ref: https://github.com/iamdustan/smoothscroll */
+
+
+let scrollLinks = document.querySelectorAll('.scrollto');
+const pageNavWrapper = document.getElementById('navigation');
+
+scrollLinks.forEach((scrollLink) => {
+
+	scrollLink.addEventListener('click', (e) => {
+
+		e.preventDefault();
+
+		let element = document.querySelector(scrollLink.getAttribute("href"));
+
+		const yOffset = 69; //page .header height
+
+		//console.log(yOffset);
+
+		const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
+
+		window.scrollTo({top: y, behavior: 'smooth'});
+
+
+		//Collapse mobile menu after clicking
+		if (pageNavWrapper.classList.contains('show')){
+			pageNavWrapper.classList.remove('show');
+		}
+
+
+    });
+
+});
+
+
+/* ===== Gumshoe SrollSpy ===== */
+/* Ref: https://github.com/cferdinandi/gumshoe  */
+// Get the sticky header
+
+
+// Initialize Gumshoe
+var spy = new Gumshoe('#navigation a', {
+	offset: 69 //page .header heights
+});
+
+
+/* ======= Countdown ========= */
+// set the date we're counting down to
+var target_date = new Date("Feb 19, 2024").getTime();
+
+// variables for time units
+var days, hours, minutes, seconds;
+
+// get tag element
+var countdown =  document.getElementById("countdown-box");
+var days_span = document.createElement("SPAN");
+days_span.className = 'days';
+countdown.appendChild(days_span);
+var hours_span = document.createElement("SPAN");
+hours_span.className = 'hours';
+countdown.appendChild(hours_span);
+var minutes_span = document.createElement("SPAN");
+minutes_span.className = 'minutes';
+countdown.appendChild(minutes_span);
+var secs_span = document.createElement("SPAN");
+secs_span.className = 'secs';
+countdown.appendChild(secs_span);
+
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+
+    // format countdown string + set tag value.
+    days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Days</span>';
+    hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
+    minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
+    secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Secs</span>';
+
+}, 1000);
